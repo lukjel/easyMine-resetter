60d5c29c5f0642f315a0d63957477d6cee3d0aad
Kuba Żeligowski <kzeligowski@MacBook-Pro-Kuba.local>
2018-05-11 21:18:00 +0200

    Initial

---
+++ cleanUp.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO
+
+GPIO.setwarnings(False)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.OUT)
+GPIO.setup(18, GPIO.OUT)
+GPIO.setup(27, GPIO.OUT)
+GPIO.setup(22, GPIO.OUT)
+GPIO.setup(23, GPIO.OUT)
+GPIO.setup(24, GPIO.OUT)
+GPIO.setup(25, GPIO.OUT)
+GPIO.setup(4, GPIO.OUT)
+GPIO.cleanup()
