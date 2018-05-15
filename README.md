18f2819b031c99d795572af947e6258fcc164119
Kuba Żeligowski <kzeligowski@MacBook-Pro-Kuba.local>
2018-05-15 23:47:10 +0200

    Script and readme

---
+++ README.md
@@ -0,0 +1,4 @@
+##Cron configuration
+
+In file `/etc/crontab` add line:
+`* * * * * root /home/pi/easyMine-resetter/scripts/launchResetter.sh`
---
+++ scripts/launchResetter.sh
@@ -0,0 +1,11 @@
+ #!/bin/bash
+
+c=`ps aux | grep -i main.py | grep -v grep | wc -l`
+s=`dirname ${0}`
+
+if [ ${c} -eq 0 ]
+then
+  cd ${s}/..
+  git pull
+  python3 main.py
+fi
