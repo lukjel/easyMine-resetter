16d6a2b1d947bad99893bf3b2d4347595b7d1064
Kuba Żeligowski <kzeligowski@MacBook-Pro-Kuba.local>
2018-05-21 22:29:12 +0200

    Dev url in env

--- env.py
+++ env.py
@@ -2,7 +2,7 @@ VENDOR = 'huncfutSzimijimmy'
 VERSION = 'test'
 MODEL = 'basic Resetter'
 SUPPORTED_COMMANDS = ["power-on", "power-off", "reset", "restart"]
-URL_BASE = 'http://192.168.1.133:8080'
+URL_BASE = 'http://dev-api.easymine.io/api'
 
 RUNTIME_PARAMETERS = {
     'TCP_PROBE_TIMEOUT': 0.001
