80775f7105cacfe360791dca9a2576e4ac128c59
Łukasz Żeligowski <lukjel@users.noreply.github.com>
2018-07-30 18:24:12 +0200

    Test

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 
 In file `/etc/crontab` add line:
 `* * * * * root /home/pi/easyMine-resetter/scripts/launchResetter.sh`
+
+Instalacja modułów:
+
+GPIO
+apt-get install python-rpi.gpio python3-rpi.gpio
+
+
+apt-get install python3-pip
+pip3 install RPi.GPIO
---
+++ test.py
@@ -0,0 +1,24 @@
+import RPi.GPIO as GPIO
+import time
+
+def pinSetup(pinNo):
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(pinNo, GPIO.OUT)
+    GPIO.output(pinNo, 1)
+
+def doPin(pinNo):
+    GPIO.output(pinNo, 0)
+    time.sleep(1)
+    GPIO.output(pinNo, 1)
+    time.sleep(1)
+    GPIO.output(pinNo, 0)
+    time.sleep(1)
+    GPIO.output(pinNo, 1)
+
+try:
+    print('Starting...')
+    pinSetup(0)
+    print('Test')
+    doPin(0)
+    
